dee085af7d26ced556edee48079699aa63315978
fcallipo <francocallipo1@gmail.com>
2022-07-20 20:26:22 -0300

    Cambios en algunos archivos y empezamos con el programa (parte del voto)

---
+++ programa.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<cmath>
+#include<fstream>
+#include<ostream>
+using namespace std;
+
+#define _TOPE_CANDIDATOS 25
+#define _TOPE_LISTAS 5
+
+struct lista {
+    int numero;
+    tNombre nombre;
+    string candidatos[_TOPE_CANDIDATOS];
+};
+
+typedef lista tLista[_TOPE_LISTAS];
+
+struct voto {
+    int numero;
+    char genero;
+    int edad;
+};
+
+void cargarVotos();
+void cargarLista(char * nombreArchivo, tLista);
+void ordenarVotos();
+void procesarVotos();
+void mostrarVotos();
+
+int main(){
+    tLista listas;
+    
+    cargarLista("candidatos.txt", listas);
+    //cargarVotos("votos.csv");
+    //ordenarVotos();
+    //mostrarVotos();
+    //procesarVotos();
+
+    cout <<listas<<endl;
+    
+}
+
+void cargarLista(char * nombreArchivo, tLista & listas) {
+
+    string nombreCandidato;
+    lista lista;
+    int i = 0;
+    int j = 0;
+    ifstream fe(nombreArchivo);
+
+    while(getline(fe, nombreCandidato)) {  
+        if(i<_TOPE_CANDIDATOS){
+            lista.candidatos[i] = nombreCandidato;
+        }
+        else{
+            lista.numero = j+1;
+            lista.nombre = "Lista", j+1;
+            listas[j] = lista;
+            i = -1;
+            j++;
+        }
+        i++;
+    }
+      
+    fe.close();
+}
+
+/*void  cargaVotos(char * ori) {
+  tRPersona per;
+  string cadena;
+  // Abre un entrada de entrada cadena de caracteres
+  ifstream fe(ori); 
+ 
+  cout<<"Procesa personas desde el archivo de texto "<<ori<<" a "<<endl;
+  cout<<"El archivo origen es un texto que tiene nombre;edad;categoria;salario "<<endl;
+  cout<<"separado por el simbolo ; sin encabezado"<<endl;
+  cout<<"============================================================="<<endl;
+  printf("%-30s %5s %5s %5s\n", "Nombre", "Edad", "Cat.", "Salario" );
+  printf("%-30s %5s %5s %5s\n", "=============================", "====", "====", "=======" );
+  while(getline(fe, cadena)) { // detecta la marca de fin de archivo
+	  parsear(cadena, per ) ; // separa los campos, los devuelve en el registro de acuerdo al tipo de datos
+
+      printf("%-30s %5d %5s %5d\n", per.nbe, per.edad, per.categoria, per.salario );
+
+   }
+   fe.close();
+}*/
