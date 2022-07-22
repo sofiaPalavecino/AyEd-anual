d27c5d054c424338b2f1c9de495a6c3f529091d7
fcallipo <francocallipo1@gmail.com>
2022-07-22 19:43:14 -0300

    Funciones hechas: Ordenar los votos y el conteo de los mismos (con todas las demas funciones que se necesitan para que estas funcionen)

--- programa.cpp
+++ programa.cpp
@@ -1,8 +1,3 @@
-// 5 partidos -> 25 candidatos cada uno
-// los votos q puso sofi
-// ordenarnlos como dijo dami
-//sistema de d'hont
-
 #include <iostream>
 #include <cmath>
 #include <fstream>
@@ -10,16 +5,22 @@
 #include <string>
 using namespace std;
 
-#define _TOPE_CANDIDATOS 25
 #define _TOPE_LISTAS 5
+#define _TOPE_CANDIDATOS 25
 #define _TOPE_VOTOS 300
 struct voto {
     int numero;
     char genero;
     int edad;
 };
+struct votoInvalido{
+    int votoNulo = 0;
+    int votoBlanco = 0;
+};
 struct lista {
     int numero;
+    int cantidadVotos;
+    float porcentajeVotosValidos;
     string nombre;
     string candidatos[_TOPE_CANDIDATOS];
 };
@@ -28,20 +29,28 @@ typedef lista tListas[_TOPE_LISTAS];
 typedef voto tVotos[_TOPE_VOTOS];
 
 void cargarLista(tListas & listas);
-void parsear(string cadenaAParsear, voto & votoParseado);
 void cargarVotos(tVotos & votos);
-/*void ordenarVotos();
-void procesarVotos();
-void mostrarVotos();*/
+void parsear(string cadenaAParsear, voto & votoParseado);
+void ordenarVotos(tVotos votos, int desde, int hasta);
+int colocar(tVotos votos, int desde, int hasta);
+void conteoVotos(tVotos votos, votoInvalido & votosInvalidos, tListas & listas);
+void sacarPromedioVotosValidos(tListas & listas, votoInvalido & votosInvalidos);
+void mostrarVotos(tVotos votos);
+void mostrarDatos(tListas listas, votoInvalido votosInvalidos);
+// void procesarVotos();
 
 int main(){
     tListas listas;
     tVotos votos;
+    votoInvalido votosInvalidos;
     
     cargarLista(listas);
     cargarVotos(votos);
-    //ordenarVotos();
-    //mostrarVotos();
+    mostrarVotos(votos);
+    ordenarVotos(votos, 0, _TOPE_VOTOS-1);
+    conteoVotos(votos, votosInvalidos, listas);
+    mostrarVotos(votos);
+    mostrarDatos(listas, votosInvalidos);
     //procesarVotos();
 }
 
@@ -53,8 +62,10 @@ void cargarLista(tListas & listas) {
 
     while(getline(archivoCandidatos, nombreCandidato)) {
         if (i == _TOPE_CANDIDATOS){
-            lista.numero = j+1;
+            lista.numero = j + 1;
             lista.nombre = "Lista " + to_string(lista.numero);
+            lista.cantidadVotos = 0;
+            lista.porcentajeVotosValidos = 0;
             listas[j] = lista;
             i = 0;
             j++;
@@ -63,27 +74,7 @@ void cargarLista(tListas & listas) {
         i++;
     }
 
-    archivoCandidatos.close();
-
-    // for (int i = 0; i < _TOPE_LISTAS; i++)
-    // {
-
-    //     cout<<"Numero de lista: " << listas[i].numero<<endl;
-    //     cout<<"Nombre de lista: " << listas[i].nombre<<endl;
-    //     cout<<"Candidatos: "<<endl;
-    //     cout<<"______________Titulares_______________"<<endl;
-    //     for (int j = 0; j < _TOPE_CANDIDATOS; j++)
-    //     {
-    //         if(j == 13){
-    //             cout<<"______________Suplentes_______________"<<endl;
-    //         }
-    //         cout<<j + 1<<") "<<listas[i].candidatos[j]<<endl;
-
-    //     }
-    //     cout<<"______________________________________"<<endl;
-    //     cout<<endl;
-
-    // }
+    archivoCandidatos.close(); 
 }
 
 void cargarVotos(tVotos & votos) {
@@ -95,13 +86,9 @@ void cargarVotos(tVotos & votos) {
     while(getline(archivoVotos, cadena)) {
         parsear(cadena, voto);
         votos[i] = voto;
-        // cout << votos[i].numero << endl;
-        // cout << votos[i].genero << endl;
-        // cout << votos[i].edad << endl;
-        // cout <<"-----------------------"<<endl;
-
         i++;
     }
+
     archivoVotos.close();
 }
 
@@ -110,14 +97,119 @@ void parsear(string cadenaAParsear, voto & votoParseado) {
     string palabra[20];
 
     while(ultimo < cadenaAParsear.size()){
-        ultimo = cadenaAParsear.find(";", primero); //cuando no lo encuentra devuelve -1
+        ultimo = cadenaAParsear.find(";", primero); // cuando no lo encuentra devuelve -1
         if (ultimo == -1) ultimo = cadenaAParsear.size();
         palabra[k] = cadenaAParsear.substr(primero, ultimo - primero) + '\0';
         k++;
         primero = ultimo + 1;
     }
 
-     votoParseado.numero = stoi(palabra[0]);
-     votoParseado.genero = palabra[1][0]; 
-     votoParseado.edad = stoi(palabra[2]);
+    votoParseado.numero = stoi(palabra[0]);
+    votoParseado.genero = palabra[1][0]; 
+    votoParseado.edad = stoi(palabra[2]);
+}
+
+void ordenarVotos(tVotos votos, int desde, int hasta){
+    int pivote;
+
+    if(desde < hasta){
+        pivote = colocar(votos, desde, hasta);
+        ordenarVotos(votos, desde, pivote - 1);
+        ordenarVotos(votos, pivote + 1, hasta);
+    }
+}
+
+int colocar(tVotos votos, int desde, int hasta){
+    int i, pivote = desde;
+    voto temp, valor_pivote = votos[pivote];
+
+    for (i = desde + 1; i <= hasta; i++){
+        if (votos[i].numero < valor_pivote.numero){
+            pivote++;
+            temp = votos[i];
+            votos[i] = votos[pivote];
+            votos[pivote] = temp;
+        }
+    }
+
+    temp = votos[desde];
+    votos[desde] = votos[pivote];
+    votos[pivote] = temp;
+    return pivote;
+}
+
+void conteoVotos(tVotos votos, votoInvalido & votosInvalidos, tListas & listas){
+    votosInvalidos.votoNulo = 0;
+    votosInvalidos.votoBlanco = 0;
+
+    for (int i = 0; i < _TOPE_VOTOS; i++){
+        if(votos[i].numero < 0 or votos[i].numero > _TOPE_LISTAS){
+            votosInvalidos.votoNulo += 1;
+        }
+        else if(votos[i].numero == 0){
+            votosInvalidos.votoBlanco += 1;
+        }
+        else{
+            for (int j = 0; j < _TOPE_LISTAS; j++){
+                if(votos[i].numero == listas[j].numero){
+                    listas[j].cantidadVotos += 1;
+                    j = _TOPE_LISTAS;
+                }
+            }
+        }
+    }
+
+    sacarPromedioVotosValidos(listas, votosInvalidos);
+}
+
+void sacarPromedioVotosValidos(tListas & listas, votoInvalido & votosInvalidos){
+    float votosValidos = _TOPE_VOTOS - votosInvalidos.votoNulo - votosInvalidos.votoBlanco;
+
+    for (int i = 0; i < _TOPE_LISTAS; i++){
+        listas[i].porcentajeVotosValidos = listas[i].cantidadVotos*100/votosValidos;
+    }
+}
+
+void mostrarVotos(tVotos votos){
+    for (int i = 0; i < _TOPE_VOTOS; i++){   
+        if(i + 1 == _TOPE_VOTOS){
+            cout<< votos[i].numero;
+        }
+        else{
+            cout<< votos[i].numero<<", ";
+        }
+    }
+    cout<<endl;
+    cout<<endl;
+}
+
+void mostrarDatos(tListas listas, votoInvalido votosInvalidos){
+
+    cout<<"______________________________________"<<endl;
+    cout<<endl;
+    cout<<"Votos en Blanco: "<<votosInvalidos.votoBlanco<<endl;
+    cout<<"Votos en Nulos: "<<votosInvalidos.votoNulo<<endl;
+    cout<<"______________________________________"<<endl;
+    cout<<endl;
+
+    for (int i = 0; i < _TOPE_LISTAS; i++){
+
+        cout<<"Numero de la lista: " << listas[i].numero<<endl;
+        cout<<"Nombre de la lista: " << listas[i].nombre<<endl;
+        cout<<"Cantidad de votos de la lista: " << listas[i].cantidadVotos<<endl;
+        cout<<"Cantidad de votos validos: " << listas[i].porcentajeVotosValidos<<"%"<<endl;
+        cout<<"Candidatos: "<<endl;
+        cout<<"______________Titulares_______________"<<endl;
+        for (int j = 0; j < _TOPE_CANDIDATOS; j++)
+        {
+            if(j == 13){
+                cout<<"______________Suplentes_______________"<<endl;
+            }
+            cout<<j + 1<<") "<<listas[i].candidatos[j]<<endl;
+
+        }
+        cout<<"______________________________________"<<endl;
+        cout<<endl;
+
+    }
 }
